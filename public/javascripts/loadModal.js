99846d119722f594c16c24ac1a2501776c6ad9ad
Kimi Zou <kimizou.kz@gmail.com>
2021-03-11 03:07:23 -0500

    Style add chore modal form; Update readme gif

--- public/javascripts/loadModal.js
+++ public/javascripts/loadModal.js
@@ -1,10 +1,12 @@
 const addChoreButton = document.querySelector(".dashboard-col-2__add-chore-button");
 
 addChoreButton.addEventListener("click", () => {
+    document.querySelector(".modal-wrapper").classList.remove("hidden");
     document.querySelector(".modal").classList.remove("hidden");
 });
 
 const closeModal = document.querySelector(".modal__close");
 closeModal.addEventListener("click", () => {
+    document.querySelector(".modal-wrapper").classList.add("hidden");
     document.querySelector(".modal").classList.add("hidden");
 })
